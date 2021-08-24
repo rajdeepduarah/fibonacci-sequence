fef5fe86e3e74668e2e592661d9b9a0ae8c5a610
rajdeepduarah <89378584+rajdeepduarah@users.noreply.github.com>
2021-08-24 12:10:53 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+=int(input("enter the range you want fibonacci sequence"))
+a=0
+b=1
+if n==1:
+    print(a)
+else:
+    print(a)
+    print(b)
+    for i in range(2,n):
+          c=a+b
+          a=b
+          b=c
+          print(c)
